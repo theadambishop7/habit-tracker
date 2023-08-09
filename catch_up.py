465583dc1ac09010c4c0920242f8e8531542b3ee
theadambishop7 <theadambishop@gmail.com>
2023-08-09 15:25:12 -0700

    Updated.

--- catch_up.py
+++ catch_up.py
@@ -14,22 +14,22 @@ pixela_header = {"X-USER-TOKEN": TOKEN}
 
 potential_hours = [1.5, 1, 1.5, 2, 2.5, 2, 2, 2, 2, 2.5, 2.5, 2.5, 3, 3, 3, 3.5, 4, 2.5]
 
-for n in range(1, 31):
-    if n < 9:
-        day = "0" + str(n + 1)
-    else:
-        day = str(n + 1)
-    date = f"202304{day}"
-
-    value_config = {
-        "date": date,
-        "quantity": str(random.choice(potential_hours))
-    }
-
-    try:
-        response = requests.post(pixela_value_endpoint, headers=pixela_header, json=value_config)
-    except:
-        print(f"Failed on day {n + 1}")
-    else:
-        print(response.json())
+# for n in range(1, 31):
+#     if n < 9:
+#         day = "0" + str(n + 1)
+#     else:
+#         day = str(n + 1)
+#     date = f"202304{day}"
+#
+#     value_config = {
+#         "date": date,
+#         "quantity": str(random.choice(potential_hours))
+#     }
+#
+#     try:
+#         response = requests.post(pixela_value_endpoint, headers=pixela_header, json=value_config)
+#     except:
+#         print(f"Failed on day {n + 1}")
+#     else:
+#         print(response.json())
 
